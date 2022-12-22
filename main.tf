bd5c20f198182262862b468b93f79c433f3c2d8c
timbob666 <badpipesdm@gmail.com>
2022-12-22 19:59:21 +0000

    initial

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-084e8c05825742534"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
